111f3b2c98cdf1f71d160286e832e15c9a1bc5d4
aleql <harambedcc@gmail.com>
2017-11-07 19:04:26 -0300

    Merge branch 'master' of https://github.com/RRayo/Tarea2CC4102
    # Conflicts:
    #	src/Experimentos/AbstractExperimento.java
    Agregada la clase ExperimentoSearchMiss

--- src/Experimentos/ExperimentoSearchMiss.java
+++ src/Experimentos/ExperimentoSearchMiss.java
@@ -1,4 +1,113 @@
 package Experimentos;
 
-public class ExperimentoSearchMiss {
+import Dictionary.IDiccionarioStruct;
+import LinearProbing.LinearProbing;
+import PatriciaTrie.PatriciaTrie;
+import TernaryTree.ABTree;
+import TextTools.TextTools;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+/**
+ * Clase para el experimento de searchMiss.
+ */
+public class ExperimentoSearchMiss extends  AbstractExperimento{
+
+    /**
+     * Campo estatico con un arerglo de palabras no presentes en lso textos.
+     */
+    private static ArrayList<String> palabrasSearchMiss = TextTools.searchMissTexts("searchMiss.txt");
+
+    /**
+     * Constructor predeterminado para el experimento de searchMiss.
+     **/
+    public ExperimentoSearchMiss() { }
+
+    /**
+     * Constructor para el experimento de searchMiss.
+     * @param fileName Nombre del archivo a generar con los resultados.
+     * @param factor Factor por el que se multiplicará la cantidad de palabras para asegurar
+     *               el 40% máximo de llenado de hash. (Recomendado 2.5)
+     * @param ini Inicio del rango de archivos (minimo 10).
+     * @param fin Fin del rango de archivos (máximo 20).
+     */
+    public ExperimentoSearchMiss (String fileName, double factor, int ini, int fin) { //factor para elegir el k en base al n
+        String[] directorios = new String[11];
+        ArrayList<String> lines = new ArrayList<>();
+        lines.add("------ RESULTADOS EXPERIMENTO Search Miss------");
+        Path file = Paths.get(fileName + ".txt");
+
+        for(int i = ini; i <= fin; i++) {
+            directorios[i-10] = String.valueOf((int)java.lang.Math.pow(2,i));
+
+        }
+
+
+        for(String dir : directorios) {
+            if(dir == null) {
+                continue;
+            }
+
+            ArrayList<String> palabrasMissExperiment = new ArrayList(palabrasSearchMiss.subList(0, Integer.parseInt(dir)/10));
+            System.err.println("Tamaño del archivo: " + dir);
+            lines.add("Tamaño: " + dir);
+
+            ArrayList<String> palabras = TextTools.leerArchivo(dir + "/x00.txt");
+
+            int n = palabras.size();
+            int k = (int)(n * factor)+1;
+
+            LinearProbing hashLinearProbing = new LinearProbing(k);
+            IDiccionarioStruct abTree = new ABTree();
+            IDiccionarioStruct patriciaTree = new PatriciaTrie();
+
+            lines.add(" ->Tiempo de construccion de linearProbing: " + super.timeTesting(hashLinearProbing, palabras, "LinearProbing"));
+            lines.add(" ->Tamaño  de linearProbing: " + String.valueOf(hashLinearProbing.getSize()));
+            double elementosHash = hashLinearProbing.elementos;
+            double porcentajeLlenado = elementosHash / n; //TODO revisar si sirve
+            lines.add(" ->Porcentaje de llenado de tabla hash:\t" + porcentajeLlenado);
+
+            lines.add(" ->Tiempo de construccion de abTree: " + super.timeTesting(abTree, palabras, "ABTree"));
+            lines.add(" ->Tamaño  de abTree: " + String.valueOf(abTree.getSize()));
+
+            lines.add(" ->Tiempo de construccion de patriciaTree: " + super.timeTesting(patriciaTree, palabras, "PatriciaTree"));
+            lines.add(" ->Tamaño  de patriciaTree: " + String.valueOf(patriciaTree.getSize()));
+
+
+            lines.add(" ->Tiempo de search Miss de linearProbing: ");
+            String[] search = super.searchTesting(hashLinearProbing, palabrasMissExperiment, "LinearProbing").split("\n");
+            for (String line: search) {
+                lines.add("\t" + line);
+            }
+            lines.add(" ->Tiempo de search Miss de abTree: ");
+            search = super.searchTesting(abTree, palabrasMissExperiment, "ABTree").split("\n");
+            for (String line: search) {
+                lines.add("\t" + line);
+            }
+            lines.add(" ->Tiempo de search Miss de patriciaTree: ");
+            search = super.searchTesting(patriciaTree, palabrasMissExperiment, "PatriciaTree").split("\n");
+            for (String line: search) {
+                lines.add("\t" + line);
+            }
+        }
+        try {
+            Files.write(file, lines, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void main (String [ ] args) {
+        ExperimentoSearchMiss e = new ExperimentoSearchMiss("testing",2.5, 10,10);
+
+    }
 }
--- src/TextTools/TextTools.java
+++ src/TextTools/TextTools.java
@@ -1,6 +1,10 @@
 package TextTools;
 
 import Dictionary.IDiccionarioStruct;
+import Experimentos.AbstractExperimento;
+import Experimentos.ExperimentoBusqueda;
+import Experimentos.ExperimentoSearchMiss;
+import PatriciaTrie.PatriciaTrie;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -70,31 +75,64 @@ public class TextTools {
 	public static void llenarDiccionario(IDiccionarioStruct d, ArrayList<String> palabras, String s) { //String s $ para arboles, "" para hash
 		int i = 0;
 		for (String p : palabras) {
-			if(p.equals("yabadaba")){
-				System.out.println("Meh");
-			}
 			d.insertar(p + s, i++);
 		}
 		System.out.println(i);
 	}
 
 
+	/**
+	 * Método para obtener un arreglo con palabras no presentes en los textos.
+	 * @param archivo Archivo del que se extraeran las palabras.
+	 * @return Retorna un arreglo de palabras.
+	 */
+	public static ArrayList<String> searchMissTexts(String archivo) {
+
+		ArrayList<String> palabras = new ArrayList<String>();
+		IDiccionarioStruct patriciaTree = new PatriciaTrie();
+		AbstractExperimento abs = new ExperimentoSearchMiss();
+
+		ArrayList<String> hobbit = TextTools.leerArchivo("/1048576" + "/x00.txt");
+		abs.timeTesting(patriciaTree, hobbit, "PatriciaTree");
+
+
+		String regex = "[^a-zA-Z ]"; //cualquier caracter que no sea letra o espacio
+			Path path2 = FileSystems.getDefault().getPath("texts", archivo);
+			try (BufferedReader reader = Files.newBufferedReader(path2, StandardCharsets.UTF_8)) {
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					line = line.toLowerCase();
+					//line = line.replaceAll("[\'\"]+", ""); //borra las comillas
+					line = line.replaceAll(regex, " "); //reemplaza todos los caracteres que no son letras por espacios
+					line = line.trim().replaceAll(" +", " "); //reemplaza multiples espacios por 1
+					String[] lineas = line.split(" "); //separa por los espacios
+					if(lineas[0].length() == 0) {
+						continue;
+					}
+					for (String p: lineas) {
+						if(patriciaTree.buscar(p + "$").size() == 0) { //no esta en el hobbit
+							palabras.add(p);
+						}
+
+					}
+				}
+			} catch (IOException x) {
+				System.err.format("IOException: %s%n", x);
+			}
+
+
+		return palabras;
+
+	}
+
+
 	//Cuenta las palabras de los archivos
 	public static void main (String [ ] args) {
-		String[] directorios = new String[11];
-		for(int i = 10; i <= 20; i++) {
-			directorios[i-10] = String.valueOf((int)java.lang.Math.pow(2,i));
-
+		System.out.println(searchMissTexts("searchMiss.txt").size());
 		}
 
-		for(String dir : directorios) {
-			if (dir == null) {
-				continue;
-			}
-			System.out.println(dir+"/x00.txt" + "\t n° palabras: \t" + TextTools.countWords(dir+"/x00.txt"));
-			System.out.println(dir+"/x01.txt" + "\t n° palabras: \t" + TextTools.countWords(dir+"/x01.txt"));
-		}
-	}
-	
+
+
+
 	
 }
